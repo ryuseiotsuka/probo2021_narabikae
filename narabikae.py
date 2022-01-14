b576dae782553ac2cd8252577cd4b228877b6f10
RYUSEI OTSUKA <50820803+ryuseiotsuka@users.noreply.github.com>
2022-01-15 01:29:03 +0900

    Add files via upload

--- narabikae.py
+++ narabikae.py
@@ -11,53 +11,60 @@ import matplotlib.pyplot as plt
 class Q_learning(object):
     def __init__(self):
         # 並び替える数列
-        self.sequence = [3,2,4,1,5]
+        #self.sequence = [3,2,4,1,5]
         self.action_space = 4
         self.done = False
-        self.max_episode = 10000#エピソード数
-        self.steps = 500#ステップ数
+        self.max_episode = 1000#エピソード数
+        self.steps = 100#ステップ数
         self.alpha = 0.8#ステップサイズ
         #できうる数列は5の階乗
-        self.q_table = np.random.uniform(low=-1,high=1,size=(5**5,self.action_space))
-        
+        self.q_table = np.random.uniform(low=0,high=1,size=(5**5,self.action_space))
+        #描画用変数
         self.x = []
         self.y = []
         
     def moving_average(self, x, w):
+        #描画用の移動平均導出
         return np.convolve(x, np.ones(w), 'valid') / w
     
     def action(self,a):
+        #数列の任意の桁とその隣を入れ替える
         temp = self.sequence[a+1]
         self.sequence[a+1] = self.sequence[a]
         self.sequence[a] = temp
     
     def decide_action(self,next_state,episode,q_table):
+        #ϵ-greedy方策
         f_prob = 0.75
         epsilon = f_prob * (1/(episode+1))
         if epsilon <= np.random.uniform(0,1):
             next_action = np.argmax(q_table[next_state])
         else:
-            prob = sum(q_table[next_state]+100)
-            w = (q_table[next_state]+100)/prob
-            next_action = np.random.choice(range(4), p=w)
+            next_action = np.random.choice(range(4))
         return next_action
             
     def get_state(self):
+        #各数列の値を状態に変換
         state = (self.sequence[0]-1)*625+                (self.sequence[1]-1)*125+                (self.sequence[2]-1)*25+                (self.sequence[3]-1)*5+                (self.sequence[4]-1)*1
         return state
     
     def check(self):
+        #数列が[1,2,3,4,5]のときのみTrueにする
         if self.sequence == [1,2,3,4,5]:
             return True
         else :
             return False
         
     def update_Qtable(self,state,action,reward,next_state):
+        #Qテーブルの更新
         max_q_table = max(self.q_table[next_state])
         #print(state,action,reward,next_state)
         self.q_table[state,action] = (1-self.alpha) * self.q_table[state,action] + self.alpha * (reward + max_q_table) 
     
     def reward(self,done):
+        #報酬
+        #並び替えが成功→1
+        #そのほか→0
         if done == True:
             reward = 1
         else :
@@ -65,22 +72,26 @@ class Q_learning(object):
         return reward
         
     def run(self):
+        #実行
         for episode in range(self.max_episode):
-            self.sequence = random.sample(self.sequence, 5)
+            #エージェントの初期化
+            print("-------------------")
+            self.sequence = [3,2,4,1,5]
             state = self.get_state()
             action = np.argmax(self.q_table[state])
             for i in range(self.steps):
-                #print(self.sequence)
-                self.action(action)
-                done = self.check()
+                print(self.sequence)
+                self.action(action)#数列の入れ替え
+                done = self.check()#並び替えができているか確認
                 reward = self.reward(done)
                 next_state = self.get_state()
-                self.update_Qtable(state,action,reward,next_state)
-                action = self.decide_action(next_state,episode,self.q_table)
+                self.update_Qtable(state,action,reward,next_state)#Qテーブルの更新
+                action = self.decide_action(next_state,episode,self.q_table)#ϵ-greedy方策を使った行動決定
                 state = next_state
                 if done:
-                    print("episode: {0} steps: {1}".format(episode,i))
                     break
+            #学習結果の表示
+            print("episode: {0} steps: {1}".format(episode,i))
             self.x.append(episode)
             self.y.append(i)
         self.y = self.moving_average(self.y, 100)
